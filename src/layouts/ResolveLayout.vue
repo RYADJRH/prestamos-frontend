1feb54892c635e65d3623c8969284311465c3838
Rafael Rebolledo <r>
2023-12-14 16:38:11 -0600

    fix multiple layouts in production

--- src/layouts/ResolveLayout.vue
+++ src/layouts/ResolveLayout.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { shallowRef, watch } from 'vue';
+import { markRaw, ref, watch } from 'vue';
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { useRoute } from "vue-router";
 
-const layout = shallowRef();
+const layout = ref();
 const route = useRoute();
-
 
 watch(
   () => route.meta?.layout as String | undefined,
@@ -13,12 +12,12 @@ watch(
     layout.value = undefined;
     try {
       const component =
-        metalayout && (await import(/* @vite-ignore */ `../layouts/${metalayout}.vue`));
-      layout.value = component?.default || DefaultLayout;
+        metalayout && (await import(`../layouts/${metalayout}.vue`));
+      layout.value = markRaw(component?.default || DefaultLayout);
     } catch (error) {
-      layout.value = DefaultLayout;
+      layout.value = markRaw(DefaultLayout);
     }
-  }
+  }, { immediate: true }
 );
 </script>
 <template>
